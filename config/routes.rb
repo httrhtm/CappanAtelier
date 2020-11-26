a9a6a35ec40c1b8bb62f8be3e56347d9b04d6028
httrhtm <tonton.voov888@docomo.ne.jp>
2020-11-26 22:04:11 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllerts: {
     registrations: 'users/registrations',
-    :sessions => 'users/sessions'
+    sessions: 'users/sessions'
   }
   root "tops#index"
+
 end
