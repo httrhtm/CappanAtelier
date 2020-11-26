0f49fc72f7fb9102db96e827f6ff34833645cd2e
httrhtm <tonton.voov888@docomo.ne.jp>
2020-11-26 22:06:10 +0900

    コントローラーの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
   end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path # ログアウト後に遷移するpathを設定
+  end
 end
